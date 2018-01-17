5dc2a837080c52f3d6afd5c0896ccdcd022471b1
Alexander Klaiber <alex.klaiber@gmail.com>
2018-01-17 13:43:22 +0100

    Add railtie initializer json_factory.jfactory_watcher

--- lib/json_factory/railtie.rb
+++ lib/json_factory/railtie.rb
@@ -2,8 +2,20 @@
 
 module JSONFactory
   class Railtie < Rails::Railtie
-    initializer :json_factory do
+    initializer 'json_factory.cache' do |app|
       Cache.instance.store = Rails.cache
+    end
+
+    initializer 'json_factory.jfactory_watcher' do |app|
+      jfactory_reloader = app.config.file_watcher.new(Dir.glob(::Rails.root.join('app/views/**/*.jfactory'))) do
+        TemplateStore.instance.clear
+      end
+
+      app.reloaders << jfactory_reloader
+
+      config.to_prepare do
+        jfactory_reloader.execute_if_updated
+      end
     end
   end
 end
